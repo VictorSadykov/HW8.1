ad891111814cf9524afaa153e99de4491584204b
Vitya Sadykov <vityasadykov@gmail.com>
2022-02-05 21:46:14 +0700

    done

--- Program.cs
+++ Program.cs
@@ -1,12 +1,61 @@
 ﻿using System;
-
+using System.IO;
 namespace HW8._1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string path = @"C:\test";
+
+            DeleteUnderMinutes(path, 30);
+        }
+
+        static void DeleteUnderMinutes(string path, int minutes)
+        {
+            var directory = new DirectoryInfo(path);
+
+            if (directory.Exists == true)
+            {
+                var dirs = directory.GetDirectories();
+                var files = directory.GetFiles();                
+
+                foreach (var dir in dirs)
+                {
+                    DeleteUnderMinutes(dir.FullName, minutes);
+                    if (DateTime.Now - dir.CreationTime < TimeSpan.FromMinutes(30))
+                    {
+                        try
+                        {
+                            dir.Delete();
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine(e.Message);
+                        }
+                    }
+
+                }
+
+
+                foreach (var file in files)
+                {
+                    if (DateTime.Now - file.CreationTime < TimeSpan.FromMinutes(30))
+                    {
+                        try
+                        {
+                            file.Delete();
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine(e.Message);
+                        }
+                    }
+
+                }
+            }
+
+            
         }
     }
 }
